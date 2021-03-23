a6277cf63e220c03db897c936eb50d7ccb2b1208
yaowen <71026994+ggfgh@users.noreply.github.com>
2021-03-24 01:23:46 +0800

    Add files via upload

---
+++ python/continue.py
@@ -0,0 +1,9 @@
+mysum = 0
+i = 0
+while i < 10:
+            i += 1
+            if i % 2 == 0:
+                continue
+            mysum += i
+
+print(mysum)
---
+++ python/daying.py
@@ -0,0 +1,10 @@
+import printstar
+
+#print(help(printstar.p_star))
+#print(dir(printstar))
+#print(help(printstar))
+
+star20 = printstar.p_star()
+print(star20)
+print("       hello")
+print(star20)
